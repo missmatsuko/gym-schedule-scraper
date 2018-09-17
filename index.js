0d8de5ba4135df6abaccdaa26e3503e252c38a6c
Matsuko Friedland <info@matsuko.ca>
2018-09-16 21:31:05 -0700

    Clean up comments and rename variables for clarity

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Modules
-const dotenv = require('dotenv').config(); // Get variables from .env
-const https = require('https'); // Require built-in HTTPS Node module
-const fs = require('fs'); // Require writeFileSync to create calendar files
-const ics = require('ics') // Require ics to create ics calendar files
-const jsdom = require("jsdom"); // Require jsdom for dom parsing
-const { JSDOM } = jsdom;
-const luxon = require('luxon'); // Require Luxon to parse and format dates
-const DateTime = luxon.DateTime;
-const RRule = require('rrule').RRule;
+// Built-in Node Modules
+const https = require('https'); // To make https request
+const fs = require('fs'); // To create files
 
-// Get HTML from schedule page
+// NPM Modules
+const dotenv = require('dotenv').config(); // To get variables from .env
+const ics = require('ics') // To ics-formatted data
+const jsdom = require("jsdom"); // To parse as HTML like a DOM
+const { JSDOM } = jsdom;
+const luxon = require('luxon'); // To parse and format dates
+const DateTime = luxon.DateTime;
+const RRule = require('rrule').RRule; // To create RRULE-formatted string
+
+// Variables
+const numberOfDaysInAWeek = 7;
+
+// Make https request to the schedule page
 https.get(process.env.GYM_SCHEDULE_PAGE, (res) => {
   // Set up empty string to store HTML
   let data = '';
@@ -19,74 +24,75 @@ https.get(process.env.GYM_SCHEDULE_PAGE, (res) => {
     data += chunk;
   });
 
-  // Console log the HTML
+  // When all chunks are received
   res.on('end', () => {
-    // Parse the HTML string
+    // Parse HTML string like a DOM
     const { document } = (new JSDOM(data)).window;
 
-    // Get selected schedule's name to later parse the schedule's start and end dates.
-    const scheduleName = document.querySelector('.schedule-selector option[selected]').textContent; // NOTE: The date range only appears on the page in this select filter
+    // Get elements on page that contain data needed for calendar events
+    const scheduleRangeElement = document.querySelector('.schedule-selector option[selected]'); // Contains name of schedule and its date range
+    const scheduleDayElements = document.querySelectorAll('#Weekly-Schedule-View>ul>li'); // For each day of the week; contains list of classes
 
-    // Get elements containing info for each day of the week
-    const dayElements = document.querySelectorAll('#Weekly-Schedule-View>ul>li'); // NOTE: dayElements don't have any classes or IDs to target
-
-    // Create an array of a week of events formatted for input into ics
-    const startEndDates = scheduleName.match(/((\d){2}\/){2}(\d){4}/g).map((date) => {
+    // Get start and end dates of range
+    const scheduleRangeDates = scheduleRangeElement.textContent.match(/((\d){2}\/){2}(\d){4}/g).map((date) => {
       return DateTime.fromFormat(date, 'dd/LL/yyyy');
     });
-    const startDate = startEndDates[0];
-    const endDate = startEndDates[1].endOf('day');
-    const startDayOfWeek = startDate.weekday;
+    const scheduleRangeStartDate = scheduleRangeDates[0];
+    const scheduleRangeEndDate = scheduleRangeDates[1].endOf('day');
+    const scheduleRangeStartDayOfWeek = scheduleRangeStartDate.weekday;
 
-    // Create RRULE
+    // Create RRULE for events in this schedule
     const rrule = new RRule({
       freq: RRule.WEEKLY,
-      until: endDate,
+      until: scheduleRangeEndDate,
     }).toString();
 
-    const events = [...dayElements].reduce((acc, dayElement) => {
-      // Get elements containing class info
-      const classElements = dayElement.querySelectorAll('ul li'); // NOTE: classElements don't have any classes or IDs to target
+    // Create an array of events formatted for input into ics
+    const events = [...scheduleDayElements].reduce((acc, scheduleDayElement) => {
+      // Create key info for each day in the schedule
+      const scheduleDayDayOfWeek = DateTime.fromFormat(scheduleDayElement.querySelector('h4').textContent, 'cccc').weekday; // Day of week
+      const scheduleDayOffset = scheduleDayDayOfWeek - scheduleRangeStartDayOfWeek + (scheduleDayDayOfWeek >= scheduleRangeStartDayOfWeek ? 0 : numberOfDaysInAWeek); // Get number of days after the start date of schedule that the schedule date occurs
+      const scheduleDate = scheduleRangeStartDate.plus({days: scheduleDayOffset}); // Get the first date in the schedule for that weekday
 
-      // Create key info
-      const dayOfWeek = DateTime.fromFormat(dayElement.querySelector('h4').textContent, 'cccc').weekday;
-      const dayOffset = dayOfWeek - startDayOfWeek + (dayOfWeek >= startDayOfWeek ? 0 : 7);
-      const date = startDate.plus({days: dayOffset});
+      // Get elements containing info about each class within each day in the schedule
+      const classElements = scheduleDayElement.querySelectorAll('ul li');
+
+      // Format classes for ics
       const classes = [...classElements].map((classElement) => {
-
-        // Get class info from HTML
-        const name = classElement.querySelector('.class-name').textContent.trim();
-        const times = classElement.querySelector('.time').textContent.trim().split(' - ').map((time) => {
+        // Get info about each class from HTML
+        const className = classElement.querySelector('.class-name').textContent.trim();
+        const classTimes = classElement.querySelector('.time').textContent.trim().split(' - ').map((time) => {
           return DateTime.fromFormat(time, 'h:mma');
         });
-        const room = classElement.querySelector('.room').textContent.trim();
-        const url = classElement.querySelector('a').href;
+        const classRoom = classElement.querySelector('.room').textContent.trim();
+        const classURL = classElement.querySelector('a').href;
 
-        // Calculate start and end
-        const start = [date.year, date.month, date.day, times[0].hour, times[0].minute];
-        const end = [date.year, date.month, date.day, times[1].hour, times[1].minute];
+        // Get start and end times of class
+        const classStartTime = classTimes[0];
+        const classEndTime = classTimes[1];
 
         return {
-          title: name,
-          location: room,
-          description: url,
-          start,
-          end,
+          title: className,
+          location: classRoom,
+          description: classURL,
+          start: [scheduleDate.year, scheduleDate.month, scheduleDate.day, classStartTime.hour, classStartTime.minute],
+          end: [scheduleDate.year, scheduleDate.month, scheduleDate.day, classEndTime.hour, classEndTime.minute],
         };
       });
 
       return acc.concat(classes);
     }, []);
 
-    // Create ics calendar file
+    // Create ics-formatted data
     ics.createEvents(events, (error, value) => {
       if (error) {
         console.log(error)
       }
 
-      // Add RRULE to each event
+      // Add RRULE to each event because ics package currently doesn't support RRULE (repeating events)
       value = value.replace(/^END:VEVENT$/gm, `${rrule}\nEND:VEVENT`);
 
+      // Write ics-formatted data to an ics file
       fs.writeFileSync(`${__dirname}/output/calendar.ics`, value);
     });
 
